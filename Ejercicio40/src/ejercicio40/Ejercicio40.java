ecfdcad96b3d1a1600124eba53564840cddfd3d0
Sergio Romero <seromeros@unal.edu.co>
2023-05-28 21:52:04 -0500

    EstructuraRepetitiva

---
+++ Ejercicio40/src/ejercicio40/Ejercicio40.java
@@ -0,0 +1,10 @@
+package ejercicio40;
+
+public class Ejercicio40 {
+
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+    }
+    
+}
---
+++ Ejercicio41/src/ejercicio41/Ejercicio41.java
@@ -0,0 +1,10 @@
+package ejercicio41;
+
+public class Ejercicio41 {
+
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+    }
+    
+}
